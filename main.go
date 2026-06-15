20bbf5e199d6b4015ef0654992627f6ce74749ea
arosyj <arosyj@126.com>
2026-06-15 11:41:56 +0800

    reorigin

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package main
 
 import "github.com/arosyjc/goclaw/cmd"
-
+import "github.com/arosyjc/goclaw/gateway/server"
 func main() {
 	cmd.Execute()
 }
